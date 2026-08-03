07a4d2ee4aefb0fff757e0a5df5d609c641b3e2c
alessiopizzi258 <alessiopizzi258@gmail.com>
2026-08-03 12:07:17 +0200

    Usa variabili d'ambiente sicure

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -2,14 +2,13 @@ import { initializeApp, getApps } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD9lIZxPHyPvJyAly1LJiSihieQNdoTA04",
-  authDomain: "beauty-hub-37ba8.firebaseapp.com",
-  projectId: "beauty-hub-37ba8",
-  storageBucket: "beauty-hub-37ba8.firebasestorage.app",
-  messagingSenderId: "877208845836",
-  appId: "1:877208845836:web:59142a789f59a527e93829"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIRE_APP_ID || process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
-// Inizializza Firebase (evita doppie inizializzazioni)
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApps()[0];
 export const db = getFirestore(app);
